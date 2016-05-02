99666140b6be83f7dc0e366352a7d5d3e8fbfdd8
Bruno Costa <doravante2@gmail.com>
2016-05-02 20:16:06 -0300

    Building scripts

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+i686-elf-as boot.s -o boot.o
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o mos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
